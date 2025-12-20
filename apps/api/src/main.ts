7ff230cd3177f02e77f5baf9fbddb8fa52f5f76a
Renol Machado <renol94@gmail.com>
2025-12-20 13:30:47 -0300

    Add HealthChecker component to check connection between front and api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [/^http:\/\/localhost:(.*)/],
+    methods: ['GET'],
+  });
+
   await app.listen(process.env.PORT ?? 8001);
 }
 bootstrap();
