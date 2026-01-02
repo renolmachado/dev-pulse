2cefd23c7b0f7659698e9428bb432bdc2f58752c
Renol Machado <renol94@gmail.com>
2026-01-02 13:18:07 -0300

    Remove AppService and clean up AppModule: eliminate unused AppService from the application module to streamline the codebase.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { HealthCheckModule } from '@repo/health-check';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NewsModule } from './modules/news/news.module';
 import { ArticlesModule } from './modules/articles/articles.module';
 
@@ -14,6 +13,6 @@ import { ArticlesModule } from './modules/articles/articles.module';
     ArticlesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
